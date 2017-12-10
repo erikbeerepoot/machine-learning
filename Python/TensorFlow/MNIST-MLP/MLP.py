a26e8dfdef2fd521d273248a9ca1fb97fa9db8ac
Erik Beerepoot <erikbeerepoot@hush.com>
2017-12-10 07:44:52 -0800

    Add basic multilayerperceptron example using MNIST dataset

---
+++ Python/TensorFlow/MNIST-MLP/MLP.py
@@ -0,0 +1,26 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
+
+x = tf.placeholder(tf.float32,shape=[None,784])
+y_ = tf.placeholder(tf.float32,shape=[None,10])
+
+W = tf.Variable(tf.zeros([784,10]),name="weights")
+b = tf.Variable(tf.zeros([10]),name="biases")
+
+session = tf.InteractiveSession()
+session.run(tf.global_variables_initializer())
+
+y = tf.matmul(x,W) + b
+
+xent = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(xent)
+
+for _ in range(1000):
+    batch = mnist.train.next_batch(100)
+    train_step.run(feed_dict={x: batch[0],y_: batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+print(accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
---
+++ Python/TensorFlow/MNIST-MLP/README.md
@@ -0,0 +1,8 @@
+## Running this example
+
+Make sure you have virtualenv and virtualenvwrapped installed. Further, make sure you have a pip3 > 8.1 installed.
+```
+mkvirtualenv tensorflow
+workon tensorflow
+pip3 install -r requirements.txt
+```
